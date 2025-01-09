c8ba187bca9d790141e42f700204b7f2f38c39ad
takokan <takokan.dev@gmail.com>
2025-01-09 23:46:31 +0530

    Added inital setup

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv"
+
+import authRoutes from './routes/auth.route.js'
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.use("/api/auth", authRoutes)
+
+app.listen(port, () => {
+    console.log("server is running on port: ", port);
+    connectDB();
+})
