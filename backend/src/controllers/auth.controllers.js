d1966a8e72a4efbb73abed71e1aae285689cb355
takokan <takokan.dev@gmail.com>
2025-01-10 23:48:44 +0530

    added login and logout controllers

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
 
+
 export const signup = async (req, res) => {
   const {fullName, email, password} = req.body;
   try {
@@ -31,6 +32,7 @@ export const signup = async (req, res) => {
         _id: newUser._id,
         fullName: newUser.fullName,
         email: newUser.email,
+        password: newUser.password,
         profilePic: newUser.profilePic,
       })
     } else {
@@ -41,10 +43,39 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Internal server error" })
   }
 };
+
 export const logout = (req, res) => {
-  res.send("logout route");
+  try {
+    res.cookie("uid", "", {maxAge:0});
+    res.status(200).json({message: "logged out sucessfully"});
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({message: "Internal Server Error"});
+  }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  const {email, password} = req.body;
+  try {
+    const user = await User.findOne({email});
+    if(!user)
+      return res.status(400).json({message: "Invalid Credentials"});
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if(!isPasswordCorrect)
+      res.status(400).json({message: "Invalid Credentials"});
+    
+    // generate token and sent it as cookie
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({message: "Internal Server Error"});
+  }
 };
